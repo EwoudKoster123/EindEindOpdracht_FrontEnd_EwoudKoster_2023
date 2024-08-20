94eea6e8e4113fe1ccb61dcef3d6455adffd523e
EwoudKoster123 <ewoudbasketbal@gmail.com>
2024-08-20 15:37:03 +0200

    javascript/css aangepast

--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -1,82 +1,98 @@
 import React, { useState } from 'react';
-import axios from "axios";
-import { useHistory } from "react-router-dom";
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 import styles from './Signup.module.css';
+import useRedirectButton from '../helpers/useRedirectButton';
 
 function Signup() {
     const [email, setEmail] = useState('');
     const [userName, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [user, setRole] = useState('');
-
     const history = useHistory();
+    const redirectToProfile = useRedirectButton('/profile');
 
-    async function SignUpUser(e) {
+
+    const SignUpUser = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post('https://frontend-educational-backend.herokuapp.com/api/auth/signup', {
-                "username": userName,
-                "email": email,
-                "password": password,
-                "role": [user]
+                username: userName,
+                email: email,
+                password: password,
+                role: [user]
             });
             console.log(response);
-            history.push("/login");
-        } catch (e) {
-            console.error(e);
+            history.push('/login');
+        } catch (error) {
+            console.error('Error during signup:', error);
+        }
+    };
+
+    function handleLoginClick() {
+        if (password.length >= 6) {
+            SignUpUser();
+            redirectToProfile();
+        } else {
+            console.error('Het wachtwoord moet minimaal 6 tekens lang zijn!');
         }
     }
 
     return (
-        <>
-            <form className={styles["form-login"]} onSubmit={SignUpUser}>
-                <label className={styles["label-email"]} htmlFor="email">
+        <div className={styles["SignupContainer"]}>
+            <h2 className={styles["SignupHeader"]}>Aanmelden</h2>
+            <form className={styles["SignupForm"]} onSubmit={SignUpUser}>
+                <label className={styles["SignupLabel"]} htmlFor="email">
                     Email:
-                    <input className={styles["input-email"]}
-                           type="email"
-                           id="email"
-                           onChange={(e) => setEmail(e.target.value)}
-                           value={email}
-                           placeholder="Fill in your email"
+                    <input
+                        className={styles["SignupInput"]}
+                        type="email"
+                        id="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        placeholder="Vul je email in"
                     />
                 </label>
-                <label className={styles["label-username"]} htmlFor="username">
+                <label className={styles["SignupLabel"]} htmlFor="username">
                     Username:
-                    <input className={styles["input-username"]}
-                           type="text"
-                           id="username"
-                           onChange={(e) => setUsername(e.target.value)}
-                           value={userName}
-                           placeholder="Fill in your username"
+                    <input
+                        className={styles["SignupInput"]}
+                        type="text"
+                        id="username"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={userName}
+                        placeholder="Vul je gebruikersnaam in"
                     />
-                    {userName.length < 6 && <p className={styles["error-message"]}>Je username is niet lang genoeg.</p>}
-                    {userName.length >= 6 && <p className={styles["good-message"]}>Je wachtwoord is lang genoeg.</p>}
+                    {userName.length < 6 && <p className={styles["ErrorMessage"]}>Je gebruikersnaam is niet lang genoeg</p>}
+                    {userName.length >= 6 && <p className={styles["GoodMessage"]}>Je gebruikersnaam is lang genoeg</p>}
                 </label>
-                <label className={styles["label-password"]} htmlFor="password">
+                <label className={styles["SignupLabel"]} htmlFor="password">
                     Password:
-                    <input className={styles["input-password"]}
-                           type="password"
-                           id="password"
-                           onChange={(e) => setPassword(e.target.value)}
-                           value={password}
-                           placeholder="Your password"
+                    <input
+                        className={styles["SignupInput"]}
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        placeholder="Vul je wachtwoord in"
                     />
-                    {password.length < 6 && <p className={styles["error-message"]}>Je username is niet lang genoeg.</p>}
-                    {password.length >= 6 && <p className={styles["good-message"]}>Je wachtwoord is lang genoeg.</p>}
+                    {password.length < 6 && <p className={styles["ErrorMessage"]}>Je wachtwoord is niet lang genoeg</p>}
+                    {password.length >= 6 && <p className={styles["GoodMessage"]}>Je wachtwoord is lang genoeg</p>}
                 </label>
-                <label className={styles["label-role"]} htmlFor="role">
+                <label className={styles["SignupLabel"]} htmlFor="role">
                     Role:
-                    <input className={styles["input-role"]}
-                           type="text"
-                           id="role"
-                           onChange={(e) => setRole(e.target.value)}
-                           value={user}
-                           placeholder="user or admin"
+                    <input
+                        className={styles["SignupInput"]}
+                        type="text"
+                        id="role"
+                        onChange={(e) => setRole(e.target.value)}
+                        value={user}
+                        placeholder="gebruiker of admin"
                     />
                 </label>
-                <button className={styles["button"]} type="submit">Register</button>
+                <button className={styles["SignupButton"]} type="submit" onClick={handleLoginClick}>Register</button>
             </form>
-        </>
+        </div>
     );
 }
 
